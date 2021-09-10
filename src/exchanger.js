5e148ca02e936a66d7362ca38b7ad3629add73a2
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-10 10:20:36 -0700

    add promise for exchanger

---
+++ src/exchanger.js
@@ -0,0 +1,14 @@
+export default class CurrencyExchange {
+  static getExchange(targetCurrency,amountToConvert) {
+    return fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${amountToConvert}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
+}
