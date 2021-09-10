e918095ab47d29b495ee5e1146ac70dace059a29
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-10 15:23:10 -0700

    throw erros if unsupported currency is used

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,14 +1,32 @@
 export default class CurrencyExchange {
   static getExchange(targetCurrency,amountToConvert) {
-    return fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${amountToConvert}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${amountToConvert}`)
       .then(function(response) {
         if (!response.ok) {
-          throw Error(response.statusText);
+          throw Error("This currency doesn't exist!");
         }
         return response.json();
       })
       .catch(function(error) {
         return error;
-      })
+      });
   }
 }
+
+// export default class CurrencyExchange {
+//   static getExchange(targetCurrency,amountToConvert) {
+//     return new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${amountToConvert}`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(request.response);
+//         }
+//       }
+//       request.open("GET", url, true);
+//       request.send();
+//     })
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './exchanger.js';
 
+function clearFields() {
+  $('.showBaseCurrency').text("");
+  $('.showTargetCurrency').text("");
+  $('.showErrors').text("");
+}
+
 function getElements(response) {
   if (response.result === "success") {
-    $('.showBaseCurrency').text(`${Math.ceil(response.conversion_result/response.conversion_rate)} USD`);
     $('.showTargetCurrency').text(`is equal to ${response.conversion_result} ${response.target_code}`);
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
@@ -17,8 +22,10 @@ $(document).ready(function() {
   $('#convertCurrency').click(function() {
     let valueToConvert = $('#amount').val();
     let targetCurrency = $('#targetCurrency').val();
+    clearFields();
     CurrencyExchange.getExchange(targetCurrency,valueToConvert)
       .then(function(response) {
+        $('.showBaseCurrency').text(`${valueToConvert} USD`);
         getElements(response);
       });
   });
