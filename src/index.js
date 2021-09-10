d13f354c0ac888b8aaf183989c3469ea4d46cac4
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-10 11:56:42 -0700

    WIP: add functionality of UI

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './exchanger';
 
 function getElements(response) {
   if (response.result === "success") {
-    $('.')
+    $('.showBaseCurrency').text(`${response.conversion_result/response.conversion_rate} USD`);
+    $('.showTargetCurrency').text(`is equal to ${response.conversion_result} ${response.target_code}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
+
+$(document).ready(function() {
+  $('#convertCurrency').click(function() {
+    let valueToConvert = $('#amount').val();
+    let targetCurrency = $('#targetCurrency').val();
+    CurrencyExchange.getExchange(targetCurrency,valueToConvert)
+      .then(function(response) {
+        getElements(response);
+      });
+  });
+});
