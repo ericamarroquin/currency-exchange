b2f394576904ddb74aa384e24f78846a8924ac28
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-10 13:53:53 -0700

    WIP: add exceptions for unknown currency

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import CurrencyExchange from './exchanger.js';
 
 function getElements(response) {
   if (response.result === "success") {
-    $('.showBaseCurrency').text(`${response.conversion_result/response.conversion_rate} USD`);
+    $('.showBaseCurrency').text(`${Math.ceil(response.conversion_result/response.conversion_rate)} USD`);
     $('.showTargetCurrency').text(`is equal to ${response.conversion_result} ${response.target_code}`);
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
